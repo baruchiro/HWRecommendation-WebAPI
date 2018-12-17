5d548f2c5a1cfe49228a3b078b1cbeef6afd46d8
Baruch Rothkoff <baruchiro@gmail.com>
2018-12-17 13:35:03 +0200

    Try to test

--- HWWebApi/Models/Memory.cs
+++ HWWebApi/Models/Memory.cs
@@ -3,7 +3,6 @@ namespace HWWebApi.Models
     public class Memory
     {
         public long Id { get; set; }
-
         public int Capacity { get; set; }
         public RAMType type { get; set; }
         public long ghz { get; set; }
