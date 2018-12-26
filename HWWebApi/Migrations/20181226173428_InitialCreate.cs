50e8b1727efc7efe07ebdf63a59b1425687d995f
Baruch Rothkoff <baruchiro@gmail.com>
2018-12-26 20:11:06 +0200

    Add migration

---
+++ HWWebApi/Migrations/20181226173428_InitialCreate.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HWWebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MotherBoards",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ddrSockets = table.Column<int>(nullable: false),
+                    maxRam = table.Column<long>(nullable: false),
+                    sataConnections = table.Column<int>(nullable: false),
+                    architacture = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MotherBoards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Processors",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    ghz = table.Column<long>(nullable: false),
+                    numOfCores = table.Column<int>(nullable: false),
+                    architacture = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Processors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Computers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    processorId = table.Column<long>(nullable: true),
+                    motherBoardId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Computers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Computers_MotherBoards_motherBoardId",
+                        column: x => x.motherBoardId,
+                        principalTable: "MotherBoards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Computers_Processors_processorId",
+                        column: x => x.processorId,
+                        principalTable: "Processors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Disks",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    type = table.Column<int>(nullable: false),
+                    rpm = table.Column<int>(nullable: false),
+                    capacity = table.Column<long>(nullable: false),
+                    ComputerId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Disks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Disks_Computers_ComputerId",
+                        column: x => x.ComputerId,
+                        principalTable: "Computers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GPUs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    cores = table.Column<int>(nullable: false),
+                    ComputerId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GPUs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GPUs_Computers_ComputerId",
+                        column: x => x.ComputerId,
+                        principalTable: "Computers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "memories",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Capacity = table.Column<int>(nullable: false),
+                    type = table.Column<int>(nullable: false),
+                    ghz = table.Column<long>(nullable: false),
+                    ComputerId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_memories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_memories_Computers_ComputerId",
+                        column: x => x.ComputerId,
+                        principalTable: "Computers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Computers_motherBoardId",
+                table: "Computers",
+                column: "motherBoardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Computers_processorId",
+                table: "Computers",
+                column: "processorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Disks_ComputerId",
+                table: "Disks",
+                column: "ComputerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GPUs_ComputerId",
+                table: "GPUs",
+                column: "ComputerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_memories_ComputerId",
+                table: "memories",
+                column: "ComputerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Disks");
+
+            migrationBuilder.DropTable(
+                name: "GPUs");
+
+            migrationBuilder.DropTable(
+                name: "memories");
+
+            migrationBuilder.DropTable(
+                name: "Computers");
+
+            migrationBuilder.DropTable(
+                name: "MotherBoards");
+
+            migrationBuilder.DropTable(
+                name: "Processors");
+        }
+    }
+}
