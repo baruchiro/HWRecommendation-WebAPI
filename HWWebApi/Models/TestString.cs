f2ad308a8a143d0ba941cab5fe4a08cbf976bc6a
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-01-22 21:46:11 +0200

    Add TestString Model Controller and Migration

---
+++ HWWebApi/Models/TestString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HWWebApi.Models
+{
+    public class TestString
+    {
+        public long Id { get; set; }
+        public String Data { get; set; }
+    }
+}
