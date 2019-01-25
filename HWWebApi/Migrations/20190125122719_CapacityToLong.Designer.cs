06c250d1bd0c43f02f779d7f4e0ea93fa0d9b3bd
Baruch Rothkoff <baruchiro@gmail.com>
2019-01-25 14:28:39 +0200

    Add migration CapacityToLong

--- HWWebApi/Migrations/20190125122719_CapacityToLong.Designer.cs
+++ HWWebApi/Migrations/20190125122719_CapacityToLong.Designer.cs
@@ -4,14 +4,16 @@ using HWWebApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HWWebApi.Migrations
 {
     [DbContext(typeof(HardwareContext))]
-    partial class HardwareContextModelSnapshot : ModelSnapshot
+    [Migration("20190125122719_CapacityToLong")]
+    partial class CapacityToLong
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HWWebApi/Migrations/20190125122719_CapacityToLong.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HWWebApi.Migrations
+{
+    public partial class CapacityToLong : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Capacity",
+                table: "Memories",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Capacity",
+                table: "Memories",
+                nullable: false,
+                oldClrType: typeof(long));
+        }
+    }
+}
