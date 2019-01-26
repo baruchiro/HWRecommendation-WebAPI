38aab3b6d2241f62f8c077ccd1a48391a2eb142a
Baruch Rothkoff <baruchiro@gmail.com>
2019-01-26 21:30:04 +0200

    Add DDR enums

--- HWWebApi/Models/Enums.cs
+++ HWWebApi/Models/Enums.cs
@@ -21,20 +21,40 @@ namespace HWWebApi.Models
     [JsonConverter(typeof(StringEnumConverter))]
     public enum RamType
     {
-        /// <summary>
-        /// DDR3
-        /// </summary>
+        [EnumMember] Other,
+        [EnumMember] Unknown,
+        [EnumMember] DRAM,
+        [EnumMember] EDRAM,
+        [EnumMember] VRAM,
+        [EnumMember] SRAM,
+        [EnumMember] RAM,
+        [EnumMember] ROM,
+        [EnumMember] FLASH,
+        [EnumMember] EEPROM,
+        [EnumMember] FEPROM,
+        [EnumMember] EPROM,
+        [EnumMember] CDRAM,
+        [EnumMember(Value = "3DRAM")] _3DRAM,
+        [EnumMember] SDRAM,
+        [EnumMember] SGRAM,
+        [EnumMember] RDRAM,
+        [EnumMember] DDR,
+        [EnumMember] DDR2,
+        [EnumMember(Value = "DDR2 FB-DIMM")] DDR2_FB_DIMM,
+        [EnumMember] Reserved,
         [EnumMember] DDR3,
-
-        /// <summary>
-        /// DDR4
-        /// </summary>
-        [EnumMember] DDR4
+        [EnumMember] FBD2,
+        [EnumMember] DDR4,
+        [EnumMember] LPDDR,
+        [EnumMember] LPDDR2,
+        [EnumMember] LPDDR3,
+        [EnumMember] LPDDR4,
     }
 
     [JsonConverter(typeof(StringEnumConverter))]
     public enum DiskType
     {
+        [EnumMember] Unknown,
         /// <summary>
         /// SSD
         /// </summary>
