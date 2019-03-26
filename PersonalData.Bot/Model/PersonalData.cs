29cf582a7d800030a2897dc77b0ca9a07bb68b11
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-03-26 19:38:49 +0200

    Fix previous commit and adding Enum extensions

---
+++ PersonalData.Bot/Model/PersonalData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PersonalData.Bot.Interfaces;
+
+namespace PersonalData.Bot.Model
+{
+    internal class PersonalData : IPersonalData
+    {
+        public string Name { get; set; }
+        public int Gender { get; set; }
+        public int Age { get; set; }
+        public string Work { get; set; }
+    }
+}
