816971092b7e95bb69e621085a3271d0ff5c3cc3
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-03-27 21:40:15 +0200

    Add migration RenameAndAddFields

---
+++ HWWebApi/Migrations/20190327193749_RenameAndAddFields.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HWWebApi.Migrations
+{
+    public partial class RenameAndAddFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Manufacturer",
+                table: "MotherBoards",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Product",
+                table: "MotherBoards",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BankLabel",
+                table: "Memories",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeviceLocator",
+                table: "Memories",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Manufacturer",
+                table: "MotherBoards");
+
+            migrationBuilder.DropColumn(
+                name: "Product",
+                table: "MotherBoards");
+
+            migrationBuilder.DropColumn(
+                name: "BankLabel",
+                table: "Memories");
+
+            migrationBuilder.DropColumn(
+                name: "DeviceLocator",
+                table: "Memories");
+        }
+    }
+}
