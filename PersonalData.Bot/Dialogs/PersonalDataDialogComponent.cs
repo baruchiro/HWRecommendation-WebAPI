ae7c64fec00efd631a35475d6a30223f434a5103
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-04-07 17:20:32 +0300

    Split request and handle steps

--- PersonalData.Bot/Dialogs/PersonalDataDialogComponent.cs
+++ PersonalData.Bot/Dialogs/PersonalDataDialogComponent.cs
@@ -5,9 +5,7 @@ using HW.Bot.Model;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Choices;
-using Microsoft.Bot.Schema;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,20 +19,25 @@ namespace HW.Bot.Dialogs
         private readonly WorkTextPrompt _workPrompt = new WorkTextPrompt(WORK_TEXT_DIALOG);
 
         private readonly IDbContext _dbContext;
-        private const string WATERFALL_DIALOG = nameof(PersonalDataDialogComponent) + "waterfall";
+        private const string WATERFALL_DIALOG = nameof(PersonalDataDialogComponent) + nameof(WaterfallDialog) + "newuser";
         private const string GENDER_CHOICE_DIALOG = nameof(PersonalDataDialogComponent) + nameof(EnumChoicePrompt<Gender>) + "gender";
         private const string AGE_NUMBER_DIALOG = nameof(PersonalDataDialogComponent) + nameof(AgeNumberPrompt) + "age";
         private const string WORK_TEXT_DIALOG = nameof(PersonalDataDialogComponent) + nameof(WorkTextPrompt) + "work";
         private const string dataID = nameof(PersonalDataDialogComponent) + "data";
 
-        public PersonalDataDialogComponent(string dialogId, IDbContext dbContext, IPersonalData personalData = null) : base(dialogId)
+        public PersonalDataDialogComponent(string dialogId, IDbContext dbContext) : base(dialogId)
         {
             _dbContext = dbContext;
+
             AddDialog(new WaterfallDialog(WATERFALL_DIALOG)
-                .AddStep(RequestGenderStep)
-                .AddStep(RequestAgeStep)
+                .AddStep(NewUserStartStep)
+                .AddStep(_genderPrompt.PromptAsync)
+                .AddStep(HandleGenderStep)
+                .AddStep(_agePrompt.PromptAsync)
+                .AddStep(HandleAgeStep)
                 .AddStep(RequestWorkStep)
-                .AddStep(SaveAndPrintAllDetailsStep)
+                .AddStep(HandleWorkStep)
+                .AddStep(SaveDetailsStep)
             );
 
             AddDialog(_genderPrompt);
@@ -42,51 +45,53 @@ namespace HW.Bot.Dialogs
             AddDialog(_workPrompt);
         }
 
-        private async Task<DialogTurnResult> RequestGenderStep(WaterfallStepContext stepcontext, CancellationToken cancellationtoken)
+        private async Task<DialogTurnResult> NewUserStartStep(WaterfallStepContext stepcontext, CancellationToken cancellationtoken)
         {
-            stepcontext.Context.Activity.CreateReply(
-                "First, we need some of your personal data to give you specific recommendations and to upgrade our system.");
+            await stepcontext.Context.SendActivityAsync(
+                "First, we need some of your personal data to give you specific recommendations and to upgrade our system.", cancellationToken: cancellationtoken);
 
             stepcontext.Values[dataID] = new PersonalData();
 
-            return await _genderPrompt.PromptAsync(stepcontext, cancellationtoken);// stepcontext.PromptAsync(CHOICE_DIALOG, promptOptions, cancellationtoken);
+            return await stepcontext.NextAsync(cancellationToken: cancellationtoken);
         }
 
-        private async Task<DialogTurnResult> RequestAgeStep(WaterfallStepContext stepcontext,
-            CancellationToken cancellationtoken)
+        private async Task<DialogTurnResult> HandleGenderStep(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            if (stepcontext.Result is FoundChoice foundChoice)
+            if (stepContext.Result is FoundChoice foundChoice)
             {
-                var personalData = stepcontext.Values[dataID] as IPersonalData ?? new Model.PersonalData();
+                var personalData = stepContext.Values[dataID] as IPersonalData ?? new PersonalData();
                 Enum.TryParse<Gender>(foundChoice.Value, true, out var result);
                 personalData.Gender = (int)result;
 
-                stepcontext.Values[dataID] = personalData;
+                stepContext.Values[dataID] = personalData;
             }
 
-            return await _agePrompt.PromptAsync(stepcontext, cancellationtoken);
+            return await stepContext.NextAsync(cancellationToken: cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> HandleAgeStep(WaterfallStepContext stepcontext, CancellationToken cancellationtoken)
+        {
+            ((IPersonalData)stepcontext.Values[dataID]).Age = stepcontext.Result is int age ? age : -1;
+
+            return await stepcontext.NextAsync(cancellationToken: cancellationtoken);
         }
 
         private async Task<DialogTurnResult> RequestWorkStep(WaterfallStepContext stepcontext, CancellationToken cancellationtoken)
         {
-            ((IPersonalData)stepcontext.Values[dataID]).Age = stepcontext.Result is int age ? age : -1;
-
             return await _workPrompt.PromptAsync(stepcontext, cancellationtoken, _dbContext.GetOrderedWorkList().Take(5));
         }
 
-        private async Task<DialogTurnResult> SaveAndPrintAllDetailsStep(WaterfallStepContext stepcontext,
-            CancellationToken cancellationtoken)
+        private async Task<DialogTurnResult> HandleWorkStep(WaterfallStepContext stepcontext, CancellationToken cancellationtoken)
         {
             ((IPersonalData)stepcontext.Values[dataID]).WorkArea = stepcontext.Result as string;
 
-            var userDetails = (IPersonalData)stepcontext.Values[dataID];
+            return await stepcontext.NextAsync(cancellationToken: cancellationtoken);
+        }
 
-            await stepcontext.Context.SendActivityAsync(
-                MessageFactory.Text($"Saving your data:\n" +
-                                    $"Gender- {userDetails.Gender.ToEnumDescription<Gender>()}\n" +
-                                    $"Age- {userDetails.Age}\n" +
-                                    $"WorkArea- {userDetails.WorkArea}"),
-                cancellationtoken);
+        private async Task<DialogTurnResult> SaveDetailsStep(WaterfallStepContext stepcontext,
+            CancellationToken cancellationtoken)
+        {
+            var userDetails = (IPersonalData)stepcontext.Values[dataID];
 
             var channelId = stepcontext.Context.Activity.ChannelId;
             var userId = stepcontext.Context.Activity.From.Id;
@@ -95,6 +100,13 @@ namespace HW.Bot.Dialogs
             {
                 await stepcontext.Context.SendActivityAsync(MessageFactory.Text("Your data saved"),
                     cancellationtoken);
+
+                await stepcontext.Context.SendActivityAsync(
+                    $"Saving your data:\n" +
+                    $"Gender- {userDetails.Gender.ToEnumDescription<Gender>()}\n" +
+                    $"Age- {userDetails.Age}\n" + 
+                    $"WorkArea- {userDetails.WorkArea}",
+                    cancellationToken: cancellationtoken);
             }
             else
             {
