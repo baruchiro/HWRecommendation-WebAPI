3ee2a1ec61102d0f405341283cc3bba1331d6b50
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-04-07 15:52:26 +0300

    Add PromptOptionsFactory to handle propmt card actions in one place

---
+++ PersonalData.Bot/Factories/PromptOptionsFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HW.Bot.Factories
+{
+    internal class PromptOptionsFactory
+    {
+        internal PromptOptions CreatePromptOptions(string text, IDictionary<string, string> keyToTitle)
+        {
+            return new PromptOptions
+            {
+                Prompt = MessageFactory.Text(text),
+                Choices = keyToTitle.Select(v =>
+                    new Choice(v.Key)
+                    {
+                        Action = new CardAction(ActionTypes.ImBack, v.Value, value: v.Key)
+                    })
+                    .ToList()
+            };
+        }
+    }
+}
