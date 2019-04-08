7751b78de1649f4864061ae4902822ab6d3b70ed
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-04-08 12:32:13 +0300

    Use Gender enum in IPersonalData

--- PersonalData.Bot/Model/PersonalData.cs
+++ PersonalData.Bot/Model/PersonalData.cs
@@ -8,7 +8,7 @@ namespace HW.Bot.Model
     internal class PersonalData : IPersonalData
     {
         public string Name { get; set; }
-        public int Gender { get; set; }
+        public Gender Gender { get; set; }
         public int Age { get; set; }
         public string WorkArea { get; set; }
     }
