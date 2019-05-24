3442b605e96e132db6bd73a82ae32270c2e4f9c1
Baruch Rothkoff <baruchiro@gmail.com>
2019-05-24 11:07:27 +0300

    Add recommendations strategies infrastructure

---
+++ ComputerUpgradeStrategies/Strategies/IDiskStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ComputerUpgradeStrategies.Recommendations;
+using Models;
+
+namespace ComputerUpgradeStrategies.Strategies
+{
+    interface IDiskStrategy
+    {
+        IEnumerable<IUpgradeRecommendation> GetRecommendations(Disk disk);
+    }
+}
