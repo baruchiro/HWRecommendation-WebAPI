95f7532428589733332e8502743c50f57788c03c
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-06-04 16:23:51 +0300

    Add RegressionBuilder project

---
+++ RegressionBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegressionBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
