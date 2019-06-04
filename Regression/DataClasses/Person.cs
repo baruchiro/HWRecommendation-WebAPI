8f2a29a02f2f5bce12b3bea468ad2072d7c9356e
Baruch Rothkoff <baruchiro@gmail.com>
2019-06-04 14:11:25 +0300

    Create Person data class

---
+++ Regression/DataClasses/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace Regression.DataClasses
+{
+    public class Person
+    {
+        [ColumnName("Age")]
+        public int Age { get; set; }
+        [ColumnName("FieldInterest")]
+        public string FieldInterest { get; set; }
+        [ColumnName("MainUse")]
+        public string MainUse { get; set; }
+        [ColumnName("Gender")]
+        public string Gender { get; set; }
+        [ColumnName("People")]
+        public int People { get; set; }
+        [ColumnName("ComputerType")]
+        public string ComputerType { get; set; }
+        [ColumnName("Price")]
+        public int Price { get; set; }
+    }
+}
