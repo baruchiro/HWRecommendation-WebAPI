fbd242d78ff6ad1b33de943319c510186df8681b
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-06-12 14:43:51 +0300

    Convert GB to float64 in KB

--- DataPreparing/src/prepare/transformers.py
+++ DataPreparing/src/prepare/transformers.py
@@ -2,8 +2,21 @@ import pandas as pd
 import re
 
 
+def __capacity_to_byte_value(capacity: pd.Series) -> int:
+    if capacity[0] is not pd.np.nan:
+        convection_relation = {'gb': 1024 * 1024, 'm': 1024, 'mb': 1024, 'kb': 1}
+        capacity = capacity[0].lower()
+        groups = re.search('(\\d+)((?:M|GB))', capacity, re.IGNORECASE)
+        count = groups.group(1)
+        unit = groups.group(2)
+        return int(count) * convection_relation[unit]
+
+
 def extract_ddr_from_gpu_processor(df: pd.DataFrame) -> pd.DataFrame:
     df["Gpu_Processor_ddr"] = df["Gpu_Processor"].str.extract('(G?DDR\\dX?)', re.IGNORECASE)
-    df["Gpu_Processor_ddr_capacity"] = df["Gpu_Processor"].str.extract('(\\d+(?:M|GB))', re.IGNORECASE)
+
+    capacity: pd.DataFrame = df["Gpu_Processor"].str.extract('(\\d+(?:M|GB))', re.IGNORECASE)
+    capacity_in_kb = capacity.apply(__capacity_to_byte_value, axis=1)
+    df["Gpu_Processor_ddr_capacity"] = capacity_in_kb
     del df["Gpu_Processor"]
     return df
