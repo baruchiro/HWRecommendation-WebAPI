cc278c9c38456544307cdbfdba41adabcbc63ea9
Baruch Rothkoff <baruchiro@gmail.com>
2019-06-12 16:46:52 +0300

    Extract processor name to model and manufacturer

--- DataPreparing/src/prepare/transformers.py
+++ DataPreparing/src/prepare/transformers.py
@@ -12,7 +12,7 @@ def __series_apply_capacity_to_byte_value(capacity: str) -> int:
             'kb': 1
         }
         capacity = capacity.lower()
-        groups = re.search('(\\d+)((?:M|GB|TB))', capacity, re.IGNORECASE)
+        groups = re.search(r'(\d+)((?:M|GB|TB))', capacity, re.IGNORECASE)
         count = groups.group(1)
         unit = groups.group(2)
         return int(count) * convection_relation[unit]
@@ -23,9 +23,9 @@ def __dataframe_apply_capacity_to_byte_value(capacity: pd.Series) -> int:
 
 
 def extract_ddr_from_gpu_processor(df: pd.DataFrame) -> pd.DataFrame:
-    df["gpu_processor_ddr"] = df["gpu_processor"].str.extract('(G?DDR\\dX?)', re.IGNORECASE)
+    df["gpu_processor_ddr"] = df["gpu_processor"].str.extract(r'(G?DDR\dX?)', re.IGNORECASE)
 
-    capacity: pd.DataFrame = df["gpu_processor"].str.extract('(\\d+(?:M|GB))', re.IGNORECASE)
+    capacity: pd.DataFrame = df["gpu_processor"].str.extract(r'(\d+(?:M|GB))', re.IGNORECASE)
     df["gpu_processor_ddr_capacity"] = capacity.apply(__dataframe_apply_capacity_to_byte_value, axis=1)
     del df["gpu_processor"]
     return df
@@ -54,4 +54,12 @@ def remove_unwanted_chars_in_processor_name(df: pd.DataFrame) -> pd.DataFrame:
     df['processor_name'] = df['processor_name'].str.replace('™', '')
     df['processor_name'] = df['processor_name'].str.replace(' Processor', '')
     df['processor_name'] = df['processor_name'].str.replace(' processor', '')
+    return df
+
+
+def extract_processor_features(df: pd.DataFrame) -> pd.DataFrame:
+    df["processor_manufacturer"] = df["processor_name"].str.extract('((?:intel|amd))', re.IGNORECASE)
+    df['processor_name'] = df['processor_name'].str.replace('((?:intel|amd)) *', '', flags=re.IGNORECASE)
+    df["processor_model"] = df["processor_name"].str.extract(r'((?:[a-z ]+)\d[- ]?\d{2,4}[a-z]{0,2})', re.IGNORECASE)
+    del df["processor_name"]
     return df
