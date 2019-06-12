71a45f2c99ed48df7a96e23c84605aad3110a86d
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-06-12 16:46:52 +0300

    Extract processor name to model and manufacturer

--- DataPreparing/src/prepare/main.py
+++ DataPreparing/src/prepare/main.py
@@ -8,7 +8,12 @@ from docopt import docopt
 
 import pandas as pd
 
-from transformers import extract_ddr_from_gpu_processor, convert_disk_capacity_to_byte, convert_memory_capacity_to_byte, convert_processor_ghz_to_mhz, remove_unwanted_chars_in_processor_name
+from transformers import extract_ddr_from_gpu_processor,\
+    convert_disk_capacity_to_byte,\
+    convert_memory_capacity_to_byte, \
+    convert_processor_ghz_to_mhz,\
+    remove_unwanted_chars_in_processor_name,\
+    extract_processor_features
 
 
 def parse_arguments() -> dict:
@@ -25,7 +30,7 @@ def read_data(source_path: str) -> pd.DataFrame:
 if __name__ == '__main__':
     arguments = parse_arguments()
     df = read_data(arguments['<input>'])
-    
+
     df.columns = [n.lower() for n in df.columns]
 
     df = extract_ddr_from_gpu_processor(df)
@@ -33,5 +38,6 @@ if __name__ == '__main__':
     df = convert_memory_capacity_to_byte(df)
     df = convert_processor_ghz_to_mhz(df)
     df = remove_unwanted_chars_in_processor_name(df)
+    df = extract_processor_features(df)
 
     df = df.reindex(sorted(df.columns), axis=1)
