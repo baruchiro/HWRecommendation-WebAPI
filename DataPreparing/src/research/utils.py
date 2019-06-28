91e18c9149ade4738e085288578b34e6322ab773
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-06-28 15:01:11 +0300

    Better correlation results

--- DataPreparing/src/research/utils.py
+++ DataPreparing/src/research/utils.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 def get_correlation_matrix(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
     types = dict(df.dtypes)
     string_columns = [c for c in types if not np.issubdtype(types[c], np.number)]
     for column in string_columns:
@@ -12,4 +13,7 @@ def get_correlation_matrix(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def correlation_matrix_to_sorted_pairs(corr: pd.DataFrame):
-    return corr.abs().stack().reset_index().sort_values([0])
+    df = corr.abs().stack().reset_index()
+    df = df.loc[(df['level_0'] != 'level_0') & (df['level_1'] != 'level_0')]
+    df = df.loc[df['level_0'] != df['level_1']]
+    return df.sort_values([0]).iloc[::2].reset_index()
