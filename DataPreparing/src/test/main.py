e76aec1809182f9d2f82c9e22d79bd966e236d28
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-06-28 16:25:47 +0300

    Test to track correlation

--- DataPreparing/src/test/main.py
+++ DataPreparing/src/test/main.py
@@ -1,10 +1,13 @@
 import unittest
 from os import path
+from typing import List, Tuple
+
 import pandas as pd
 
 from pandas.util.testing import assert_frame_equal
 
 from src.prepare.main import read_data, transpose_data
+from src.research.utils import get_correlation_matrix, correlation_matrix_to_sorted_pairs
 
 test_dir = path.dirname(__file__)
 orig_path = 'data/fake-data-orig.csv'
@@ -29,6 +32,33 @@ class TestStringMethods(unittest.TestCase):
         transposed_df = copy_types_from_another(transposed_df, out_df)
 
         assert_frame_equal(transposed_df, out_df, check_dtype=False)
+
+    def test_track_correlation(self):
+        def assert_correlation_fields_count(correlation: int, rows: int, fields: List[Tuple[str, str]]):
+            actual_rows = corr.loc[corr[0] == correlation]
+            self.assertEqual(actual_rows[0].count(), rows,
+                             f'expected {rows} rows with {correlation} correlation but found '
+                             f'{actual_rows[0].count()} rows.')
+
+            for tuple_fields in fields:
+                lines = len(actual_rows.loc[
+                            actual_rows['both'].str.contains(tuple_fields[0]) &
+                            actual_rows['both'].str.contains(tuple_fields[1])
+                            ])
+                self.assertEqual(lines, 1, f'expected exacly one rows with {tuple_fields} but found {lines} rows')
+
+        orig_df = read_data(orig_path).reset_index(drop=True)
+
+        transposed_df = transpose_data(orig_df).reset_index(drop=True)
+
+        corr = correlation_matrix_to_sorted_pairs(get_correlation_matrix(transposed_df))
+        corr[0] = (corr[0] * 10).astype(int)
+        corr['both'] = corr['level_0'] + corr['level_1']
+
+        assert_correlation_fields_count(correlation=9, rows=1, fields=[('motherboard_ddrsockets', 'computertype')])
+        assert_correlation_fields_count(correlation=8, rows=1, fields=[('disk_model', 'computertype')])
+        assert_correlation_fields_count(correlation=7, rows=5, fields=[])
+        assert_correlation_fields_count(correlation=0, rows=145, fields=[])
 
 
 if __name__ == '__main__':
