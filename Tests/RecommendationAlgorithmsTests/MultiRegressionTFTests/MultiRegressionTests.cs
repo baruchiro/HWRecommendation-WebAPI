ba493d40590cc08dcc0059ea34b2a718ca6e4764
Baruch Rothkoff <baruchiro@gmail.com>
2019-07-05 16:25:57 +0300

    Add MultiRegressionTests project

---
+++ Tests/RecommendationAlgorithmsTests/MultiRegressionTFTests/MultiRegressionTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MultiRegressionTFTests
+{
+    public class MultiRegressionTests
+    {
+        [Fact]
+        public void CreateModel()
+        {
+
+        }
+    }
+}
