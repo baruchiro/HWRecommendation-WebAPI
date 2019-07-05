0d0126de574d1d3e27bd580fa5a1f069624c701d
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-07-05 17:35:38 +0300

    Test loading data

---
+++ Tests/RecommendationAlgorithmsTests/MultiRegressionTFTests/StringHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace MultiRegressionTFTests
+{
+    class StringHandler : IFileHandle
+    {
+        private readonly Stream _streamReader;
+        private readonly Stream _streamWriter;
+
+        public StringHandler(string data)
+        {
+            _streamReader = data.ToStream();
+            _streamWriter = data.ToStream();
+        }
+
+        public void Dispose()
+        {
+            _streamWriter.Close();
+            _streamReader.Close();
+        }
+
+        public Stream CreateWriteStream()
+        {
+            return _streamWriter;
+        }
+
+        public Stream OpenReadStream()
+        {
+            return _streamReader;
+        }
+
+        public bool CanWrite => _streamWriter.CanWrite;
+        public bool CanRead => _streamReader.CanRead;
+
+
+    }
+
+    internal static class extension
+    {
+        public static Stream ToStream(this string str)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(str);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
