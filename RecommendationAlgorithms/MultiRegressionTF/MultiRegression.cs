8b6cebbabcc88b063f750638d3d62af947bb5620
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-07-08 20:27:39 +0300

    Half work

--- RecommendationAlgorithms/MultiRegressionTF/MultiRegression.cs
+++ RecommendationAlgorithms/MultiRegressionTF/MultiRegression.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Microsoft.ML;
 using Microsoft.ML.Data;
+using Microsoft.ML.Transforms;
 
 namespace MultiRegressionTF
 {
@@ -15,6 +16,8 @@ namespace MultiRegressionTF
             {"object", DataKind.String},
             {"float64", DataKind.Double}
         };
+
+        private readonly string[] _yColumns = {"age", "computertype", "fieldinterest", "gender", "mainuse", "price"};
 
         private TextLoader.Column[] ReadColumnsFromList(string[] columns)
         {
@@ -28,14 +31,37 @@ namespace MultiRegressionTF
             //Create ML Context with seed for repeteable/deterministic results
             MLContext mlContext = new MLContext(seed: 0);
 
+            var columns = ReadColumnsFromList(trainDtypesCsv);
+            var categoricalColumns = columns.Where(c => c.DataKind == DataKind.String)
+                .Select(c => new InputOutputColumnPair(c.Name));
+            var numericColumns = columns.Where(c => c.DataKind == DataKind.Int64 || c.DataKind == DataKind.Double)
+                .Select(c => new InputOutputColumnPair(c.Name));
+
             // STEP 1: Common data loading configuration
-            TextLoader textLoader = mlContext.Data.CreateTextLoader(ReadColumnsFromList(trainDtypesCsv),
+            TextLoader textLoader = mlContext.Data.CreateTextLoader(columns,
                 hasHeader: true,
                 separatorChar: ','
             );
 
-            IMultiStreamSource multiStream = new FileHandleSource(trainDataCsv);
-            IDataView fakedDataView = textLoader.Load(multiStream);
+            var fakedDataView = textLoader.Load(new FileHandleSource(trainDataCsv));
+            var split = mlContext.Data.TrainTestSplit(fakedDataView, 0.2);
+
+            var baseTrainingDataView = split.TrainSet;
+            var testDataView = split.TestSet;
+
+            // Categorical data
+            var dataProcessPipeline = mlContext.Transforms.Categorical.OneHotEncoding(categoricalColumns.ToArray())
+                    // Normalize
+                    .Append(mlContext.Transforms.NormalizeMinMax("age", "age"))
+                //.Append(mlContext.Transforms.NormalizeMeanVariance(numericColumns.ToArray()))
+                //.Append(mlContext.Transforms.Concatenate("Y", _yColumns))
+                //.Append(mlContext.Transforms.Concatenate("X",
+                //    "VendorIdEncoded", "RateCodeEncoded", "PaymentTypeEncoded", "PassengerCount", "TripTime",
+                //    "TripDistance"));
+                //.Append(mlContext.Model.LoadTensorFlowModel("Resources/saved_model.pb")
+                //    .ScoreTensorFlowModel(new[] {"Y"}, new[] {"X"}));
+                ;
+            var trainedModel = dataProcessPipeline.Fit(baseTrainingDataView);
         }
     }
 }
