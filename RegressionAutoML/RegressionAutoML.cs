386bffc9cf89418556dae0e8ec08aef3376e79e4
Baruch Rothkoff <baruchiro@gmail.com>
2019-07-09 18:50:47 +0300

    Step

--- RegressionAutoML/RegressionAutoML.cs
+++ RegressionAutoML/RegressionAutoML.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using Microsoft.ML;
+using Microsoft.ML.AutoML;
+using Microsoft.ML.Data;
 
 namespace RegressionAutoML
 {
-    class RegressionAutoML
+    public class RegressionAutoML
     {
+        public void Train(IDataView data)
+        {
+            var mlContext = new MLContext();
+
+            var cts = new CancellationTokenSource();
+            var experimentSettings = new RegressionExperimentSettings
+            {
+                MaxExperimentTimeInSeconds = 3600,
+                CancellationToken = cts.Token
+            };
+
+            var experiment = mlContext.Auto().CreateRegressionExperiment(experimentSettings);
+
+            var experimentResult = experiment
+                .Execute(data, "memory_capacity_as_kb");
+        }
     }
 }
