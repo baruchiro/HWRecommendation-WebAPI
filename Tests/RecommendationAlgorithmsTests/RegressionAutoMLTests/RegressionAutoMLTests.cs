999957da06ccc858873fcd0764e926978d74b192
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-07-11 19:44:49 +0300

    Make train more generic

--- Tests/RecommendationAlgorithmsTests/RegressionAutoMLTests/RegressionAutoMLTests.cs
+++ Tests/RecommendationAlgorithmsTests/RegressionAutoMLTests/RegressionAutoMLTests.cs
@@ -1,5 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using DataTestsUtils;
 using Microsoft.ML;
+using Microsoft.ML.AutoML;
+using Microsoft.ML.Data;
 using Xunit;
 
 namespace RegressionAutoMLTests
@@ -8,23 +13,49 @@ namespace RegressionAutoMLTests
     {
 
         [Fact]
-        public void Train()
+        public async Task TrainAsync()
         {
+            string[] labels =
+            {
+                "memory_capacity_as_kb",
+                "disk_capacity_as_kb",
+                "disk_rpm"
+            };
             var mlContext = new MLContext();
             var dataLoader = new DataLoader(mlContext);
 
+            var results = new List<RunDetail<RegressionMetrics>>();
+
+            var taskFactory = new TaskFactory<RunDetail<RegressionMetrics>>();
+            var tasks = labels.Select(l =>
+                taskFactory.StartNew(() => RunForLabel(dataLoader, l))).ToList();
+
+            while (tasks.Count > 0)
+            {
+                var finished = await Task.WhenAny(tasks);
+                tasks.Remove(finished);
+                results.Add(await finished);
+            }
+
+            foreach (var result in results)
+            {
+                var metrics = result.ValidationMetrics;
+                var r = $"R-Squared: {metrics.RSquared:0.##}";
+                var m = $"Root Mean Squared Error: {metrics.RootMeanSquaredError:0.##}";
+            }
+        }
+
+        private RunDetail<RegressionMetrics> RunForLabel(DataLoader dataLoader, string label)
+        {
             var data = dataLoader.Builder.ConvertIntToSingle()
                 .ConvertNumberToSingle()
                 .SelectFeatureColumns()
-                .SelectColumns("memory_capacity_as_kb")
+                .SelectColumns(label)
                 .GetData();
 
             var regressionAutoML = new RegressionAutoML.RegressionAutoML();
-            var result = regressionAutoML.Train(data);
-
-            var metrics = result.ValidationMetrics;
-            var r = $"R-Squared: {metrics.RSquared:0.##}";
-            var m = $"Root Mean Squared Error: {metrics.RootMeanSquaredError:0.##}";
+            var result = regressionAutoML.Train(data, label, 10);
+            return result;
         }
     }
 }
