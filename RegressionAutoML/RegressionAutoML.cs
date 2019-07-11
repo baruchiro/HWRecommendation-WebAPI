266b0c2c756369b91e7e605822c90d6d4e14e1c0
Baruch Rothkoff <baruchiro@gmail.com>
2019-07-11 19:44:49 +0300

    Make train more generic

--- RegressionAutoML/RegressionAutoML.cs
+++ RegressionAutoML/RegressionAutoML.cs
@@ -10,22 +10,21 @@ namespace RegressionAutoML
 {
     public class RegressionAutoML
     {
-        public RunDetail<RegressionMetrics> Train(IDataView data)
+        public RunDetail<RegressionMetrics> Train(IDataView data, string label, uint minutes)
         {
             var mlContext = new MLContext();
 
             var cts = new CancellationTokenSource();
             var experimentSettings = new RegressionExperimentSettings
             {
-                MaxExperimentTimeInSeconds = 1*60,
+                MaxExperimentTimeInSeconds = minutes * 60,
                 CancellationToken = cts.Token,
                 OptimizingMetric = RegressionMetric.MeanSquaredError
             };
 
             var experiment = mlContext.Auto().CreateRegressionExperiment(experimentSettings);
 
-            var experimentResult = experiment
-                .Execute(data, "memory_capacity_as_kb");
+            var experimentResult = experiment.Execute(data, label);
 
             return experimentResult.BestRun;
         }
