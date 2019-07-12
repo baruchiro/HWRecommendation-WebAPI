b4ef345d1b49c905e2ff71033f214adb0f6e06b4
Baruch Rothkoff <Baruch.Rothkoff@checkmarx.com>
2019-07-12 16:37:30 +0300

    Create environment for train models

---
+++ RecommendationAlgorithms/Trainer/Interfaces/IRecommendationAlgorithmLearner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace Trainer.Interfaces
+{
+    interface IRecommendationAlgorithmLearner
+    {
+        ILearningResult TrainModel(int timeoutInMinutes);
+        void SaveModel(string fileName);
+    }
+}
