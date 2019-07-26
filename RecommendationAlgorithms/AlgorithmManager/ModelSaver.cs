f7edfcfa684e329ac0fbce4982e7faa0cb4ccd0f
Baruch Rothkoff <baruchiro@gmail.com>
2019-07-26 17:56:22 +0300

    ModelSaver.LoadModel and test

--- RecommendationAlgorithms/AlgorithmManager/ModelSaver.cs
+++ RecommendationAlgorithms/AlgorithmManager/ModelSaver.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
 using System.Threading;
 using AlgorithmManager.Interfaces;
 using Microsoft.ML;
@@ -11,27 +13,69 @@ namespace AlgorithmManager
     {
         private readonly MLContext _mlContext;
         private readonly string _outputDir;
+        private readonly string _dateFormat = "yyMMdd-HHmmss";
+        private readonly IFileSystem _fileSystem;
 
-        public ModelSaver(MLContext mlContext, string outputDir)
+        public ModelSaver(MLContext mlContext, string outputDir, IFileSystem fileSystem = null)
         {
-            _outputDir = Directory.CreateDirectory(outputDir).FullName;
-            
+            _fileSystem = fileSystem ?? new FileSystem();
+            _outputDir = _fileSystem.Directory.CreateDirectory(outputDir).FullName;
+
             _mlContext = mlContext;
         }
 
         public void SaveModel(LearningResult learningResult)
         {
             if (learningResult == null) throw new ArgumentNullException(nameof(learningResult));
-            Thread.Sleep(TimeSpan.FromSeconds(5));
-            var filename = Path.Combine(_outputDir,
-                $"{learningResult.Name}_{new DateTime():yyMMdd_hhmmss}");
+
+            var filename = _fileSystem.Path.Combine(_outputDir,
+                $"{learningResult.Name}_{DateTime.Now.ToString(_dateFormat)}");
 
             var jsonFile = $"{filename}.json";
             var modelPath = $"{filename}.zip";
 
             var json = JsonConvert.SerializeObject(learningResult, Formatting.Indented);
-            File.WriteAllText(jsonFile, json);
+            _fileSystem.File.WriteAllText(jsonFile, json);
+
             _mlContext.Model.Save(learningResult.Model, learningResult.Schema, modelPath);
+
+            using (var stream = _fileSystem.FileStream.Create(modelPath, FileMode.Create))
+            {
+                _mlContext.Model.Save(learningResult.Model, learningResult.Schema, stream);
+            }
+        }
+
+        public LearningResult LoadModel(string modelName)
+        {
+            var newestJson = GetNewestFile(modelName, "json");
+            var newestZip = GetNewestFile(modelName, "zip");
+
+            var json = _fileSystem.File.ReadAllText(newestJson);
+            var learningResult = JsonConvert.DeserializeObject<LearningResult>(json);
+
+            using (var stream = _fileSystem.FileStream.Create(newestZip, FileMode.Open))
+            {
+                learningResult.Model = _mlContext.Model.Load(stream, out var inputSchema);
+
+                if (inputSchema != learningResult.Schema)
+                    throw new JsonSerializationException(
+                        "Input schema loaded by MLContext is different from the Input schema loaded by Json");
+            }
+
+            return learningResult;
+        }
+
+        private string GetNewestFile(string fileStartWith, string extension)
+        {
+            return _fileSystem.Directory.EnumerateFiles(_outputDir, $"{fileStartWith}_*.{extension}")
+                .OrderBy(GetModelCreationTime).First();
+        }
+
+        public DateTime GetModelCreationTime(string filename)
+        {
+            var filenameWithoutExtension = _fileSystem.Path.GetFileNameWithoutExtension(filename);
+            return DateTime.ParseExact(filenameWithoutExtension?.Split('_').Last(),
+                _dateFormat, null); 
         }
     }
 }
---
+++ Tests/RecommendationAlgorithmsTests/AlgorithmManagerTests/ModelSaverTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using System.Text;
+using AlgorithmManager;
+using AlgorithmManager.Interfaces;
+using FakeItEasy;
+using Microsoft.ML;
+using Xunit;
+
+namespace AlgorithmManagerTests
+{
+    public class ModelSaverTests
+    {
+        private MockFileSystem _fileSystem;
+        private ModelSaver _modelSaver;
+        private string _outputDir;
+
+        public ModelSaverTests()
+        {
+            var mlContext = new MLContext(0);
+            _fileSystem = new MockFileSystem();
+            _outputDir = _fileSystem.Path.GetTempPath();
+
+            _modelSaver = new ModelSaver(mlContext, _outputDir, _fileSystem);
+        }
+
+        [Fact]
+        public void SaveModel_FileSystem_AssertJsonZipDates()
+        {
+            var label = "Label123";
+            var learningResult = A.Fake<LearningResult>();
+            A.CallTo(() => learningResult.Name).Returns(label);
+
+            var now = DateTime.Now;
+            _modelSaver.SaveModel(learningResult);
+
+            var extensions = new List<string> {".zip", ".json"};
+            var files = _fileSystem.Directory.GetFiles(_outputDir, $"{label}*")
+                .Where(f => extensions.Contains(_fileSystem.Path.GetExtension(f)))
+                .ToList();
+
+            Assert.Equal(2, files.Count);
+            Assert.Equal(now, _modelSaver.GetModelCreationTime(files[0]),
+                TimeSpan.FromSeconds(1));
+            Assert.Equal(now, _modelSaver.GetModelCreationTime(files[1]), 
+                TimeSpan.FromSeconds(1));
+        }
+    }
+}
